f2aa8dde2cd9cd21efe2fe4f7e704db991d0237a
abhigupta19 <17ume005@lnmiit.ac.in>
2019-06-03 16:04:01 +0530

    Initial commit

---
+++ app/src/main/java/com/sar/user/frag/Fragmix.java
@@ -0,0 +1,17 @@
+package com.sar.user.frag;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmix extends Fragment{
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragmix,container,false);
+    }
+}
+
